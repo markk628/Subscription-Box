b49a6cb678d1439012fefeefe27555267626c674
Donghyun <markk628@gmail.com>
2020-04-24 15:52:09 -0700

    added featured cells and its constraints

--- OnboardingFlow/View/Cells/FavoritesCell.swift
+++ OnboardingFlow/View/Cells/FavoritesCell.swift
@@ -6,37 +6,49 @@
 //  Copyright © 2020 Mark Kim. All rights reserved.
 //
 
-//import UIKit
-//
-//class FavoritesCell: UICollectionViewCell {
-//    
-//    static var identifier: String = "FavoritesCell"
-//    
-//    let favoritesView: UIView = {
-//        let favView = UIView()
-//        favView.translatesAutoresizingMaskIntoConstraints = false
-//        return favView
-//    }()
-//    
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        setUp()
-//    }
-//    
-//    func setUp() {
-//        favoritesView.layer.cornerRadius = 10
-//    }
-//    
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        setUpLayout()
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    private func setUpLayout() {
-//        addSubview(<#T##view: UIView##UIView#>)
-//    }
-//}
+import UIKit
+
+class FavoritesCell: UICollectionViewCell {
+    
+    static var identifier: String = "FavoritesCell"
+    
+    let favoritesView: UIImageView = {
+        let favView = UIImageView()
+        favView.layer.cornerRadius = 10
+        favView.translatesAutoresizingMaskIntoConstraints = false
+        return favView
+    }()
+    
+    let itemLabel: UILabel = {
+        let itemName = UILabel()
+        itemName.text = "Item Name"
+        itemName.translatesAutoresizingMaskIntoConstraints = false
+        return itemName
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpLayout() {
+        addSubview(favoritesView)
+        addSubview(itemLabel)
+        
+        NSLayoutConstraint.activate([
+            favoritesView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            favoritesView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+            favoritesView.trailingAnchor.constraint(equalTo: itemLabel.leadingAnchor, constant: -5),
+            favoritesView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5),
+            
+            itemLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            itemLabel.leadingAnchor.constraint(equalTo: favoritesView.trailingAnchor, constant: 5),
+            itemLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            itemLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5)
+        ])
+    }
+}
